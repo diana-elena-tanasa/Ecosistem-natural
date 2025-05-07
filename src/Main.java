deb2317d3346cc0cf11e92034f15baab83ad4787
didihelen <tanasa.dianaelena@gmail.com>
2025-05-07 11:49:19 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("🌿 Simulare Ecosistem Natural - Pornire");
+/*
+        // Inițializare pădure
+        Padure padure = new Padure(20, 20); // ex: 20x20 coordonate
+
+        // Adăugare surse de apă
+        padure.adaugaSursaApa(new SursaApa(new Coordonate(5, 5)));
+        padure.adaugaSursaApa(new SursaApa(new Coordonate(15, 10)));
+
+        // Adăugare plante
+        padure.adaugaPlanta(new Planta(new Coordonate(3, 4)));
+        padure.adaugaPlanta(new Planta(new Coordonate(7, 12)));
+
+        // Adăugare animale
+        padure.adaugaAnimal(new Ierbivor("Caprioara", Gen.FEMININ, new Coordonate(2, 3)));
+        padure.adaugaAnimal(new Pradator("Lup", Gen.MASCULIN, new Coordonate(10, 8)));
+
+        // Setare anotimp inițial
+        Anotimp anotimpInitial = Anotimp.PRIMAVARA;
+
+        // Creare simulare
+        Simulare simulare = new Simulare(padure, anotimpInitial);
+
+        // Rulare simulare (ex: 10 pași)
+        simulare.ruleaza(10);
+
+        System.out.println("🌳 Simulare încheiată.");*/
     }
 }
